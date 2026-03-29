0cbc7aea251c9532b150379a918df7bec117dd4b
deepanshusuryan-sytem <deepanshu.suryan@eleva8e.com>
2026-03-30 00:27:37 +0530

    refresh jwt token

--- controller/userController.js
+++ controller/userController.js
@@ -53,11 +53,58 @@ export async function loginUser(req, res) {
             return res.status(401).json({ message: "Invalid Password", success: false })
         }
 
-        const accessToken = jwt.sign({ email: user.email, _id: user._id }, process.env.JWT_ACCESS_SECRET, { expiresIn: "24h" })
+        const accessToken = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_ACCESS_SECRET, { expiresIn: "15m" })
+
+        const refreshToken = jwt.sign({ email: user.email, id: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" })
+
+        user.refreshToken = refreshToken;
+        await user.save();
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
 
         return res.status(200).json({ message: "Login successful", success: true, accessToken: accessToken })
     } catch (error) {
         console.log(error);
+        return res.status(500).json({ message: "Internal server error", success: false })
+    }
+}
+
+export async function refreshAccessToken(req, res) {
+    try {
+        const token = req.cookies.refreshToken;
+        if (!token) {
+            return res.status(400).send({ message: "No refresh Token", success: false });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+
+        const user = await User.findById(decoded.id);
+        if (!user || user.refreshToken !== token) {
+            return res.status(403).json({ message: "Invalid refresh token", success: false });
+        }
+
+        const newAccessToken = jwt.sign({ email: user?.email, id: user._id }, process.env.JWT_ACCESS_SECRET, { expiresIn: "15m" })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error", success: false })
+    }
+}
+
+export async function logoutUser(req, res) {
+    try {
+        const token = req.cookies.refreshToken;
+        if (!token) {
+            return res.status(204).send(); // already logged out
+        }
+        const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+        await User.findByIdAndUpdate(decoded.id, { refreshToken: null });
+        res.clearCookie("refreshToken", { httpOnly: true, sameSite: "strict" });
+        return res.status(200).json({ message: "Logged out successfully", success: true });
+    } catch (error) {
         return res.status(500).json({ message: "Internal server error", success: false })
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {createUser, getUser, loginUser} from "../controller/userController.js"
+import {createUser, getUser, loginUser, logoutUser, refreshAccessToken} from "../controller/userController.js"
 
 const router= express.Router();
 
 router.post("/create",createUser);
 router.post("/login", loginUser);
 router.get("/getuser/:id", getUser);
+router.post("/refresh",refreshAccessToken);
+router.post("/logout", logoutUser);
 
 export { router as userRouter };
