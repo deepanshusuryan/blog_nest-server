3db079493b84abb773c7d4d68dc5a1b6c17ebac7
deepanshusuryan-sytem <deepanshu.suryan@eleva8e.com>
2026-04-14 00:48:13 +0530

    bug fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
     origin:[
-        // process.env.NEXT_PUBLIC_FRONTEND_URL,
+        process.env.NEXT_PUBLIC_FRONTEND_URL,
         "http://localhost:3000"
     ],
     credentials: true,
